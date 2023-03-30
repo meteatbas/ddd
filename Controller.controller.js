e3fa87ba1cbe36b277311a18462f50396ef18e08
metea <meteatbas@gmail.com>
2023-03-30 22:03:22 +0300

    third

--- Controller.controller.js
+++ Controller.controller.js
@@ -4,8 +4,10 @@ sap.ui.define([
 	"sap/m/MessageToast",
 	"sap/m/ToolbarSpacer",
 	"sap/ui/table/Row",
-	"sap/ui/thirdparty/jquery"
-], function(Controller, JSONModel, MessageToast, ToolbarSpacer, TableRow, jQuery) {
+	"sap/ui/thirdparty/jquery",
+	"sap/ui/core/syncStyleClass",
+	"sap/ui/core/Fragment"
+], function(Controller, JSONModel, MessageToast, ToolbarSpacer, TableRow, jQuery,syncStyleClass,Fragment) {
 	"use strict";
 
 	return Controller.extend("sap.ui.table.sample.DnD.Controller", {
@@ -17,11 +19,6 @@ sap.ui.define([
 			this.oProductsModel = this.initSampleProductsModel();
 			oView.setModel(this.oProductsModel);
 
-			// sap.ui.require(["sap/ui/table/sample/TableExampleUtils"], function(TableExampleUtils) {
-			// 	var oTb = oView.byId("infobar");
-			// 	oTb.addContent(new ToolbarSpacer());
-			// 	oTb.addContent(TableExampleUtils.createInfoButton("sap/ui/table/sample/DnD"));
-			// }, function(oError) { /*ignore*/ });
 		},
 
 		onExit: function() {
@@ -82,6 +79,76 @@ sap.ui.define([
 			var list = this.getView().byId("table1").getBinding("rows").filter(oFilter);
 			var binding = list.getBinding("rows");
 			binding.filter(aFilters, "Application");
+		},
+		onPressAddRow: function() {
+			var oModel = this.getView().getModel(),
+				line = oModel.oData.ProductCollection[1]; //Just add to the end of the table a line like the second line
+			oModel.oData.ProductCollection.push(line);
+			oModel.refresh();
+		},
+		onAdd: function (event) {                               //to add a new row
+			const model = event.getSource().getModel();
+			const newArray = model.getProperty("/ProductCollection").concat({
+				id: globalThis.crypto.randomUUID(),
+				text: "My New Item",
+			});
+			model.setProperty("/ProductCollection", newArray, null, true);
+			let line = model.oData.ProductCollection[1]
+			line.studentId=null;
+			// var oModel = this.getView().getModel()
+			model.oData.ProductCollection.push(line);
+			model.refresh()
+		},
+		onAddBtnPress: function() {
+			const dialog = this.byId("dialog");
+			syncStyleClass("sapUiSizeCompact", this.getView(), dialog);
+			dialog.open();
+		},
+		onbuttonpress: function (oEvent) {
+			var oView = this.getView();
+			// var path = oEvent.getSource().getBindingContext("table1").getPath();
+// create dialog lazily
+			if (!this.byId("openDialog")) {
+// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "inputdialog",
+					controller: this
+				}).then(function (oDialog) {
+// connect dialog to the root view
+//of this component (models, lifecycle)
+					oView.addDependent(oDialog);
+					oDialog.bindElement({
+						path: "rows",
+						model: "sample"
+					});
+					oDialog.open();
+				});
+			}
+		},
+
+		closeDialog: function () {
+			this.byId("openDialog").destroy();
+		},
+		onDialogAddPress: function() {
+			const dialog = this.byId("openDialog");
+			const isInput = control => control.isA("sap.m.InputBase");
+			const inputs = dialog.getControlsByFieldGroupId("inputs").filter(isInput);
+			const invalidInput = inputs.find(c => c.getValueState() == "Error");
+			if (invalidInput) {
+				invalidInput.focus();
+			} else {
+				// const connid = dialog.getBindingContext("rows").getProperty("connid");
+				this.getView().getModel().submitChanges({
+					groupId: "addingFlight",
+					success: this.onCreateSuccess.bind(this),
+				});
+			}
+		},
+		onCreateSuccess: function() {
+			const message = `Flight ${2} Added`;
+			sap.ui.require(["sap/m/MessageToast"], MT => MT.show(message));
+			this.byId("dialog").close();
 		}
 	});
 
